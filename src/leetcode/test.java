e903fd2caad6d1c484d1732d49a796123e03a9a7
AtanuRoy666 <atanuroy316@gmail.com>
2024-10-31 20:41:15 +0600

    ValidPalindrome - Leetcode

--- src/leetcode/test.java
+++ src/leetcode/test.java
@@ -2,12 +2,13 @@ package leetcode;
 
 public class test {
     public static void main(String[] args) {
-        LinkedList l1 = new LinkedList();
-        int [] arr = {1, 2, 3, 4, 5};
-        ListNode head = l1.makeLinkedList(arr);
-        l1.printLinkedList(head);
-        System.out.println();
-        head = l1.reverseLinkedList(head);
-        l1.printLinkedList(head);
+//        LinkedList l1 = new LinkedList();
+//        int [] arr = {1, 2, 3, 4, 5};
+//        ListNode head = l1.makeLinkedList(arr);
+//        l1.printLinkedList(head);
+//        System.out.println();
+//        head = l1.reverseLinkedList(head);
+//        l1.printLinkedList(head);
+        System.out.println(Character.toLowerCase('1'));
     }
 }
